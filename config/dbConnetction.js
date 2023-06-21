3ada90eadb335c30266aa2ec60320a49c792d0cb
ardaarslan47 <a.ardaarslan@hotmail.com>
2023-06-22 02:22:35 +0300

    mongoUrl updated

--- config/dbConnetction.js
+++ config/dbConnetction.js
@@ -2,13 +2,10 @@ const mongoose = require("mongoose");
 
 const connectDb = async () => {
   try {
-    await mongoose.connect(
-      `mongodb+srv://ardaarslan:${process.env.MONGO_PASSWORD}@cluster0.uusk2j2.mongodb.net/?retryWrites=true&w=majority`,
-      {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-      }
-    );
+    await mongoose.connect(process.env.MONGO_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
     console.log("Connected to MongoDB Atlas");
   } catch (err) {
     console.error("Error connecting to MongoDB Atlas:", err);
